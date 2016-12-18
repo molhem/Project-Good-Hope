584febdbc734c26bb166e1b96f61d3eb8385a2b3
poovamraj <poovamraj@gmail.com>
2016-12-18 17:32:22 +0530

    final iteration

---
+++ app/src/main/java/com/goodhopes/poovam/projectgoodhopes/common/OnClickHandlers.java
@@ -0,0 +1,8 @@
+package com.goodhopes.poovam.projectgoodhopes.common;
+
+/**
+ * Created by poovam on 14/12/16.
+ */
+
+public class OnClickHandlers {
+}
---
+++ app/src/main/java/com/goodhopes/poovam/projectgoodhopes/common/SplashScreen.java
@@ -0,0 +1,8 @@
+package com.goodhopes.poovam.projectgoodhopes.common;
+
+/**
+ * Created by poovam on 16/12/16.
+ */
+
+public class SplashScreen {
+}
